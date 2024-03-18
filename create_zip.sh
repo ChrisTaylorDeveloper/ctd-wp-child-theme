6942628ae89028fc9cf1c9404fbb2b9ba6cee041
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-03-18 15:50:30 +0100

    Add info about what is being removed.

--- create_zip.sh
+++ create_zip.sh
@@ -3,5 +3,5 @@
 commit=$(git rev-parse --short HEAD)
 dir_name=ctd-wp-child-theme
 filename=$dir_name-$commit.zip
-rm -f $dir_name*.zip 
+rm -fv $dir_name*.zip 
 zip -rj "$filename" ./ctd-wp-child-theme
