6e05e529636ba3b6da1ac62d0a1e232a8737eba7
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-03-18 15:43:54 +0100

    Improve zip script - do not include root folder in zip.

--- create_zip.sh
+++ create_zip.sh
@@ -4,4 +4,4 @@ commit=$(git rev-parse --short HEAD)
 dir_name=ctd-wp-child-theme
 filename=$dir_name-$commit.zip
 rm -f $dir_name*.zip 
-zip -r "$filename" ./ctd-wp-child-theme
+zip -rj "$filename" ./ctd-wp-child-theme
