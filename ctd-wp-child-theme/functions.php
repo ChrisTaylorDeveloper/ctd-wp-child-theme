a1fa8e424f957dce9e93045066c89e4426202380
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-03-19 09:48:04 +0100

    Use Bootstrap instead.

--- ctd-wp-child-theme/functions.php
+++ ctd-wp-child-theme/functions.php
@@ -1,1 +1,6 @@
 <?php
+
+function ctd_scripts() {
+    wp_enqueue_style('bs533', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+}
+add_action( 'wp_enqueue_scripts', 'ctd_scripts' );
