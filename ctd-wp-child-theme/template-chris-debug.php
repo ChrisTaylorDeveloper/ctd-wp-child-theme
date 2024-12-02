d56a984278a31abc9c10ad59ad389e74cbaa4678
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-12-02 15:56:36 +0000

    Dump some ini settings.

--- ctd-wp-child-theme/template-chris-debug.php
+++ ctd-wp-child-theme/template-chris-debug.php
@@ -3,4 +3,8 @@
 * Template Name: Chris debug
 */
 
+var_dump(ini_get( 'upload_max_size' , '256M' ));
+var_dump(ini_get( 'post_max_size', '256M' ));
+var_dump(ini_get( 'max_execution_time', '300' ));
+
 phpinfo();
