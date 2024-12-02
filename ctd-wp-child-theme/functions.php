8cd453b045a0406cc720d0307544c414093d55ef
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-12-02 16:15:55 +0000

    Attempt to set and var_dump ini settings.

--- ctd-wp-child-theme/functions.php
+++ ctd-wp-child-theme/functions.php
@@ -1,8 +1,8 @@
 <?php
 
-@ini_set( 'upload_max_size' , '256M' );
-@ini_set( 'post_max_size', '256M' );
-@ini_set( 'max_execution_time', '300' );
+// @ini_set( 'upload_max_size' , '256M' );
+// @ini_set( 'post_max_size', '256M' );
+// @ini_set( 'max_execution_time', '300' );
 
 function ctd_scripts() {
     wp_enqueue_style('bs533', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
