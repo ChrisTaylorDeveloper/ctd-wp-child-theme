8b56d3bb1c25930790fc7cdc0a61d32136f703a6
Chris Taylor <chris@christaylordeveloper.co.uk>
2024-12-02 16:28:30 +0000

    ini settings now working, from php scripts.

--- ctd-wp-child-theme/template-chris-debug.php
+++ ctd-wp-child-theme/template-chris-debug.php
@@ -3,12 +3,4 @@
 * Template Name: Chris debug
 */
 
-ini_set( 'upload_max_size' , '64M' );
-ini_set( 'post_max_size', '64M');
-ini_set( 'max_execution_time', '300' );
-
-var_dump(ini_get( 'upload_max_size' ));
-var_dump(ini_get( 'post_max_size' ));
-var_dump(ini_get( 'max_execution_time' ));
-
 phpinfo();
